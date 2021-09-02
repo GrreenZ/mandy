02229d2a100a48102492dae6a130b1502227cdaa
Andrzej Borsuk <red@szsz.pl>
2021-09-02 19:31:58 +0200

    tab->space (turbo editor compatible)

--- mandp.pas
+++ mandp.pas
@@ -2,41 +2,41 @@ program mand;
 
 function mandi(x:integer;y:integer) : integer;
 var
-	xz,yz,xx,yy,xt : real;
-	i : integer;
-	
+  xz,yz,xx,yy,xt : real;
+  i : integer;
+  
 label
-	done;
+  done;
 
 begin
-	xz := x*3.5/32.0-2.5;
-	yz := y*2.0/22.0-1.0;
-	xx := 0;
-	yy := 0;
+  xz := x*3.5/32.0-2.5;
+  yz := y*2.0/22.0-1.0;
+  xx := 0;
+  yy := 0;
 
-	for i := 0 to 14 do
-	begin
-		if (xx*xx + yy*yy > 4.0) then goto done;
-		xt := xx*xx - yy*yy + xz;
-		yy := 2.0*xx*yy + yz;
-		xx := xt
-	end;
-	
-	done:
-	mandi := i;
+  for i := 0 to 14 do
+  begin
+    if (xx*xx + yy*yy > 4.0) then goto done;
+    xt := xx*xx - yy*yy + xz;
+    yy := 2.0*xx*yy + yz;
+    xx := xt
+  end;
+  
+  done:
+  mandi := i;
 end;
 
 var
-	x,y : integer;
-	c : char;
+  x,y : integer;
+  c : char;
 begin
-	for y := 0 to 21 do
-	begin
-		for x := 0 to 31 do
-		begin
-			c := chr(mandi(x,y) + 31);
-			write(c,c)
-		end;
-		writeln
-	end
+  for y := 0 to 21 do
+  begin
+    for x := 0 to 31 do
+    begin
+      c := chr(mandi(x,y) + 31);
+      write(c,c)
+    end;
+    writeln
+  end
 end.
